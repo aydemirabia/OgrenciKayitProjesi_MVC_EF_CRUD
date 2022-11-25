086916d8cc0fc9d771abb073e83ac9dd06241cdf
Rabia Aydemir <aydemirabia@gmail.com>
2022-11-25 12:11:35 +0300

    MVC EF CRUD ile Öğrenci Kayıt Projesi
    MVC ve Entity Framework kullanarak Crud işlemlerini gerçekleştirdiğim Öğrenci Kayıt Projesi veri tabanı visual studio üzerinden oluşturulmuştur. Projeyi Index.cs sayfasından açmalısınız.

---
+++ OgrenciKayitProjesi/mvc_crud_ogrencikayitprojesi/dbo.tbl_ogr.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[OgrID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [OgrAd] VARCHAR(50) NULL, 
+    [OgrSoyad] VARCHAR(50) NULL, 
+    [OgrMail] VARCHAR(50) NULL, 
+    [OgrFotograf] VARCHAR(500) NULL, 
+    [OgrAdres] VARCHAR(50) NULL
+)
